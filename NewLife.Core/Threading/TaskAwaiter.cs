c278518031305037699dc3d66c69b24ec3561be8
智能大石头 <nnhy@vip.qq.com>
2023-03-16 23:49:19 +0800

    fix 修正AggregateException无限包装的问题

--- NewLife.Core/Threading/TaskAwaiter.cs
+++ NewLife.Core/Threading/TaskAwaiter.cs
@@ -179,6 +179,8 @@ namespace Microsoft.Runtime.CompilerServices
 
         internal static Exception PrepareExceptionForRethrow(Exception exc)
         {
+            if (exc is AggregateException agg) return agg;
+
             // 打包后再向外抛出异常，避免直接抛出打断了异常调用栈
             return new AggregateException(exc);
         }
